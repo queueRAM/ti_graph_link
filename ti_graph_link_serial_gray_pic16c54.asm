05157986a72b64cd22540c90bb8778c869b4e60c
queueRAM <queueram@proton.me>
2025-07-10 00:32:30 -0700

    Decode rest of serial gray PIC assembly

--- ti_graph_link_serial_gray_pic16c54.asm
+++ ti_graph_link_serial_gray_pic16c54.asm
@@ -86,233 +86,298 @@ LADR_0x0025:
     MOVF LRAM_0x0E,W     ; W = LRAM_0x0E
     IORLW 0xF0           ; W |= 0xF0
     MOVWF FSR            ; FSR = W
+
     MOVF LRAM_0x0A,W     ; W = LRAM_0x0A
     MOVWF INDF           ; IND = W
+
     INCF LRAM_0x0E,F     ; LRAM_0x0E += 1
-    INCF LRAM_0x0D,F     ; LRAM_0x0D +=1
-    BTFSC LRAM_0x0D,4
+    INCF LRAM_0x0D,F     ; LRAM_0x0D += 1
+
+    BTFSC LRAM_0x0D,4    ; if LRAM_0x0D[4] == 1: LRAM_0x07 |= 0'b0010_0000
       BSF LRAM_0x07,5
-    BCF LRAM_0x07,4
+
+    BCF LRAM_0x07,4      ; LRAM_0x07 &= 0'b1110_0111
     BCF LRAM_0x07,3
-    MOVLW LADR_0x0000           ;   b'00000000'  d'000'
+
+    MOVLW LADR_0x0000    ; JumpBack = LADR_0x0000
     MOVWF JumpBack
-    CLRWDT
+
+    CLRWDT               ; reset watchdog
     GOTO LADR_0x00E5
+
 LADR_0x0036:
-    BTFSS PORTA,0
+    BTFSS PORTA,0        ; if PORTA[0] == 0: GOTO LADR_0x0116
       GOTO LADR_0x0116
-    RRF LRAM_0x0A,F
+
+    RRF LRAM_0x0A,F      ; LRAM_0x0A = (LRAM_0x0A >> 1) | 0'b1000_0000
     BSF LRAM_0x0A,7
-    BSF PORTA,1
-    MOVLW LADR_0x003F           ;   b'00111111'  d'063'  "?"
+    BSF PORTA,1          ; PORTA[1] = 1
+    MOVLW LADR_0x003F    ; JumpBack = LADR_0x003F
     MOVWF JumpBack
+
 LADR_0x003D:
-    BTFSC TMR0,7
+    BTFSC TMR0,7         ; if TMR0[7] == 1: GOTO LADR_0x00E7
       GOTO LADR_0x00E7
 LADR_0x003F:
-    BTFSS PORTA,2
-      GOTO LADR_0x003D
-    BCF PORTA,1
-    MOVLW LADR_0x0048           ;   b'01001000'  d'072'  "H"
+    BTFSS PORTA,2        ; if PORTA[2] == 0: GOTO LADR_0x003D
+      GOTO LADR_0x003D   ; loop until PORTA[2] goes high
+
+    BCF PORTA,1          ; PORTA[1] = 0
+    MOVLW LADR_0x0048    ; JumpBack = LADR_0x0048
     MOVWF JumpBack
+
 LADR_0x0044:
-    BTFSC TMR0,7
+    BTFSC TMR0,7         ; if TMR0[7] == 1: GOTO LADR_0x00E7
       GOTO LADR_0x00E7
-    BTFSS PORTA,0
+    BTFSS PORTA,0        ; if PORTA[0] == 0: GOTO LADR_0x0044
       GOTO LADR_0x0044
-LADR_0x0048:
-    DECFSZ LRAM_0x0B,F
-      GOTO LADR_0x000B
-    MOVF LRAM_0x0E,W
-    IORLW 0xF0           ;   b'11110000'  d'240'
+
+LADR_0x0048:             ; literal copy from LADR_0x0025
+    DECFSZ LRAM_0x0B,F   ; LRAM_0x0B -= 1
+      GOTO LADR_0x000B   ; if LRAM_0x0B != 0: GOTO LADR_0x000B
+
+    MOVF LRAM_0x0E,W     ; FSR = LRAM_0x0E | 0xF0
+    IORLW 0xF0
     MOVWF FSR
-    MOVF LRAM_0x0A,W
+
+    MOVF LRAM_0x0A,W     ; INDF = LRAM_0x0A
     MOVWF INDF
-    INCF LRAM_0x0E,F
-    INCF LRAM_0x0D,F
-    BTFSC LRAM_0x0D,4
-    BSF LRAM_0x07,5
-    BCF LRAM_0x07,4
+
+    INCF LRAM_0x0E,F     ; LRAM_0x0E += 1
+    INCF LRAM_0x0D,F     ; LRAM_0x0D += 1
+
+    BTFSC LRAM_0x0D,4    ; if LRAM_0x0D[4] == 1: LRAM_0x07 |= 0'b0010_0000
+      BSF LRAM_0x07,5
+
+    BCF LRAM_0x07,4      ; LRAM_0x07 &= 0'b1110_0111
     BCF LRAM_0x07,3
-    MOVLW LADR_0x0000           ;   b'00000000'  d'000'
+
+    MOVLW LADR_0x0000    ; JumpBack = LADR_0x0000
     MOVWF JumpBack
-    CLRWDT
+
+    CLRWDT               ; reset watchdog
     GOTO LADR_0x00E5
 
 LADR_0x0059:
-    BTFSC LRAM_0x07,4
+    BTFSC LRAM_0x07,4    ; if LRAM_0x07[4] == 1: GOTO LADR_0x00F6
       GOTO LADR_0x00F6
-    BSF LRAM_0x07,3
-    MOVLW 0x08           ;   b'00001000'  d'008'
+
+    BSF LRAM_0x07,3      ; LRAM_0x07 |= 0'b0000_1000
+    MOVLW 0x08           ; LRAM_0x0B = 0x08
     MOVWF LRAM_0x0B
-    MOVF LRAM_0x0F,W
-    IORLW 0xF0           ;   b'11110000'  d'240'
+
+    MOVF LRAM_0x0F,W     ; FSR = LRAM_0x0F | 0xF0
+    IORLW 0xF0
     MOVWF FSR
-    MOVF INDF,W
+
+    MOVF INDF,W          ; LRAM_0x0A = INDF
     MOVWF LRAM_0x0A
-    INCF LRAM_0x0F,F
-    DECF LRAM_0x0D,F
-    BTFSC STATUS,Z
+
+    INCF LRAM_0x0F,F     ; LRAM_0x0F += 1
+    DECF LRAM_0x0D,F     ; LRAM_0x0D -= 1
+    BTFSC STATUS,Z       ; if LRAM_0x0D == 0: LRAM_0x07 |= 0'b0001_0000
       BSF LRAM_0x07,4
-    BCF LRAM_0x07,5
-    MOVF LRAM_0x0D,W
-    XORLW 0x0F           ;   b'00001111'  d'015'
-    BTFSC STATUS,Z
+
+    BCF LRAM_0x07,5      ; LRAM_0x07 &= 0'b1101_1111
+    MOVF LRAM_0x0D,W     ; W = LRAM_0x0D
+    XORLW 0x0F           ; W = W ^ 0x0F
+    BTFSC STATUS,Z       ; if W == 0: GOTO LADR_0x0071
       GOTO LADR_0x0071
-    MOVF LRAM_0x0D,W
-    XORLW 0x0E           ;   b'00001110'  d'014'
-    BTFSC STATUS,Z
+
+    MOVF LRAM_0x0D,W     ; W = LRAM_0x0D
+    XORLW 0x0E           ; W = W ^ 0x0E
+    BTFSC STATUS,Z       ; if W != 0 OR (W == 0 AND LRAM_0x07 == 0): PORTB[2] = 0
       BTFSS LRAM_0x07,0
         BCF PORTB,2
+
 LADR_0x0071:
-    RRF LRAM_0x0A,F
-    BTFSC STATUS,C
+    RRF LRAM_0x0A,F      ; STATUS.C = LRAM_0x0A[0], LRAM_0x0A = (LRAM_0x0A >> 1)
+    BTFSC STATUS,C       ; if STATUS.C != 0: GOTO LADR_0x008B
       GOTO LADR_0x008B
-    BSF PORTA,1
-    MOVLW LADR_0x0079           ;   b'01111001'  d'121'  "y"
+
+    BSF PORTA,1          ; PORTA[1] = 1
+    MOVLW LADR_0x0079    ; JumpBack = LADR_0x0079
     MOVWF JumpBack
+
 LADR_0x0077:
-    BTFSC TMR0,7
+    BTFSC TMR0,7         ; if TMR0[7] == 1: GOTO LADR_0x00F8
       GOTO LADR_0x00F8
 LADR_0x0079:
-    BTFSC PORTA,2
-      GOTO LADR_0x0077
-    BCF PORTA,1
-    MOVLW LADR_0x0080           ;   b'10000000'  d'128'
+    BTFSC PORTA,2        ; if PORTA[2] == 1: GOTO LADR_0x0077
+      GOTO LADR_0x0077   ; loop until PORTA[2] goes low
+
+    BCF PORTA,1          ; PORTA[1] = 0
+    MOVLW LADR_0x0080    ; JumpBack = LADR_0x0080
     MOVWF JumpBack
+
 LADR_0x007E:
-    BTFSC TMR0,7
+    BTFSC TMR0,7         ; if TMR0[7] == 1: GOTO LADR_0x00F8
       GOTO LADR_0x00F8
 LADR_0x0080:
-    BTFSS PORTA,0
+    BTFSS PORTA,0        ; if PORTA[0] == 0: GOTO LADR_0x0116
       GOTO LADR_0x0116
-    BTFSS PORTA,2
-      GOTO LADR_0x007E
-    DECFSZ LRAM_0x0B,F
-      GOTO LADR_0x0071
-    BCF LRAM_0x07,3
-    MOVLW LADR_0x0059           ;   b'01011001'  d'089'  "Y"
+    BTFSS PORTA,2        ; if PORTA[2] == 0: GOTO LADR_0x007E
+      GOTO LADR_0x007E   ; loop until PORTA[2] goes high
+
+    DECFSZ LRAM_0x0B,F   ; LRAM_0x0B -= 1
+      GOTO LADR_0x0071   ; if LRAM_0x0B != 0: GOTO LADR_0x0071
+    BCF LRAM_0x07,3      ; LRAM_0x07 &= 0'b1111_0111
+
+    MOVLW LADR_0x0059    ; JumpBack = LADR_0x0059
     MOVWF JumpBack
-    CLRWDT
+    CLRWDT               ; reset watchdog
     GOTO LADR_0x00F6
+
 LADR_0x008B:
-    BSF PORTA,3
-    MOVLW LADR_0x0090           ;   b'10010000'  d'144'
+    BSF PORTA,3          ; PORTA[3] = 1
+    MOVLW LADR_0x0090    ; JumpBack = LADR_0x0090
     MOVWF JumpBack
+
 LADR_0x008E:
-    BTFSC TMR0,7
+    BTFSC TMR0,7         ; if TMR0[7] == 1: GOTO LADR_0x00F8
       GOTO LADR_0x00F8
 LADR_0x0090:
-    BTFSC PORTA,0
-      GOTO LADR_0x008E
-    BCF PORTA,3
-    MOVLW LADR_0x0097           ;   b'10010111'  d'151'
+    BTFSC PORTA,0        ; if PORTA[0] == 1: GOTO LADR_0x008E
+      GOTO LADR_0x008E   ; loop until PORTA[0] goes low
+
+    BCF PORTA,3          ; PORTA[3] = 0
+    MOVLW LADR_0x0097    ; JumpBack = LADR_0x0097
     MOVWF JumpBack
+
 LADR_0x0095:
-    BTFSC TMR0,7
+    BTFSC TMR0,7         ; if TMR0[7] == 1: GOTO LADR_0x00F8
       GOTO LADR_0x00F8
 LADR_0x0097:
-    BTFSS PORTA,2
+    BTFSS PORTA,2        ; if PORTA[2] == 0: GOTO LADR_0x0116
       GOTO LADR_0x0116
-    BTFSS PORTA,0
-      GOTO LADR_0x0095
-    DECFSZ LRAM_0x0B,F
-      GOTO LADR_0x0071
-    BCF LRAM_0x07,3
-    MOVLW LADR_0x0059           ;   b'01011001'  d'089'  "Y"
+    BTFSS PORTA,0        ; if PORTA[0] == 0: GOTO LADR_0x007E
+      GOTO LADR_0x0095   ; loop until PORTA[0] goes high
+
+    DECFSZ LRAM_0x0B,F   ; LRAM_0x0B -= 1
+      GOTO LADR_0x0071   ; if LRAM_0x0B != 0: GOTO LADR_0x0071
+    BCF LRAM_0x07,3      ; LRAM_0x07 &= 0'b1111_0111
+    MOVLW LADR_0x0059    ; JumpBack = LADR_0x0059
     MOVWF JumpBack
-    CLRWDT
+    CLRWDT               ; reset watchdog
     GOTO LADR_0x00F6
+
 LADR_0x00A2:
-    BSF LRAM_0x07,0
-    MOVLW 0x1E           ;   b'00011110'  d'030'
+    BSF LRAM_0x07,0      ; LRAM_0x07 |= 0'b0000_0001
+
+    MOVLW 0x1E           ; TMR0 = 0x1E
     MOVWF TMR0
-    MOVLW 0x08           ;   b'00001000'  d'008'
+
+    MOVLW 0x08           ; LRAM_0x09 = 0x08
     MOVWF LRAM_0x09
-    MOVF LRAM_0x0D,W
-    ANDLW 0x0E           ;   b'00001110'  d'014'
-    XORLW 0x0E           ;   b'00001110'  d'014'
-    BTFSC STATUS,Z
+
+    MOVF LRAM_0x0D,W     ; W = (LRAM_0x0D & 0x0E) ^ 0x0E
+    ANDLW 0x0E
+    XORLW 0x0E
+    BTFSC STATUS,Z       ; if W == 0: PORTB[2] = 1
       BSF PORTB,2
-    MOVF JumpBack,W
-    MOVWF PCL            ; !!Program-Counter-Modification
+
+    MOVF JumpBack,W      ; GOTO JumpBack
+    MOVWF PCL
+
 LADR_0x00AE:
-    RRF LRAM_0x08,F
+    RRF LRAM_0x08,F      ; LRAM_0x08 = LRAM_0x08 >> 1
     BCF LRAM_0x08,7
-    BTFSS PORTB,0
+    BTFSS PORTB,0        ; if PORTB[0] == 0: LRAM_0x08 |= 0'b1000_0000
       BSF LRAM_0x08,7
+
     MOVF JumpBack,W
-    DECFSZ LRAM_0x09,F
-      MOVWF PCL            ; !!Program-Counter-Modification
-    MOVF LRAM_0x0E,W
-    IORLW 0xF0           ;   b'11110000'  d'240'
+    DECFSZ LRAM_0x09,F   ; LRAM_0x08 -= 1
+      MOVWF PCL          ; if LRAM_0x09 != 0: GOTO JumpBack
+
+    MOVF LRAM_0x0E,W     ; FSR = LRAM_0x0E | 0xF0
+    IORLW 0xF0
     MOVWF FSR
-    MOVF LRAM_0x08,W
+
+    MOVF LRAM_0x08,W     ; INDF = LRAM_0x08
     MOVWF INDF
-    INCF LRAM_0x0E,F
-    INCF LRAM_0x0D,F
-    BTFSC LRAM_0x0D,4
+
+    INCF LRAM_0x0E,F     ; LRAM_0x0E += 1
+    INCF LRAM_0x0D,F     ; LRAM_0x0D += 1
+
+    BTFSC LRAM_0x0D,4    ; if LRAM_0x0D[4] == 1: LRAM_0x07 |= 0'b0010_0000
       BSF LRAM_0x07,5
-    BCF LRAM_0x07,4
+
+    BCF LRAM_0x07,4      ; LRAM_0x07 &= 0'b1110_1110
     BCF LRAM_0x07,0
-    CLRWDT
-    MOVF JumpBack,W
-    MOVWF PCL            ; !!Program-Counter-Modification
+
+    CLRWDT               ; reset watchdog
+    MOVF JumpBack,W      ; GOTO JumpBack
+    MOVWF PCL
+
 LADR_0x00C3:
-    BSF LRAM_0x07,1
-    BSF PORTB,1
-    MOVF LRAM_0x0F,W
-    IORLW 0xF0           ;   b'11110000'  d'240'
+    BSF LRAM_0x07,1      ; LRAM_0x07 |= 0'b0000_0010
+    BSF PORTB,1          ; PORTB[1] = 1
+
+    MOVF LRAM_0x0F,W     ; FSR = LRAM_0x0F | 0xF0
+    IORLW 0xF0
     MOVWF FSR
-    MOVF INDF,W
+
+    MOVF INDF,W          ; LRAM_0x08 = INDF
     MOVWF LRAM_0x08
-    INCF LRAM_0x0F,F
-    DECF LRAM_0x0D,F
-    BTFSC STATUS,Z
+
+    INCF LRAM_0x0F,F     ; LRAM_0x0F += 1
+    DECF LRAM_0x0D,F     ; LRAM_0x0D -= 1
+    BTFSC STATUS,Z       ; if W == 0: LRAM_0x07 |= 0'b0001_0000
       BSF LRAM_0x07,4
-    BCF LRAM_0x07,5
-    MOVLW 0x08           ;   b'00001000'  d'008'
+    BCF LRAM_0x07,5      ; LRAM_0x07 &= 0'b1101_1111
+
+    MOVLW 0x08           ; LRAM_0x09 = 0x08
     MOVWF LRAM_0x09
+
     MOVF JumpBack,W
-    MOVWF PCL            ; !!Program-Counter-Modification
+    MOVWF PCL            ; GOTO JumpBack
+
 LADR_0x00D3:
-    BTFSC LRAM_0x07,2
+    BTFSC LRAM_0x07,2    ; if LRAM_0x07[2] == 1: GOTO LADR_0x00DF
       GOTO LADR_0x00DF
-    RRF LRAM_0x08,F
-    BTFSC STATUS,C
-      BCF PORTB,1
-    BTFSS STATUS,C
-      BSF PORTB,1
+    RRF LRAM_0x08,F      ; STATUS.C = LRAM_0x08[0], LRAM_0x08 = LRAM_0x08 >> 1
+    BTFSC STATUS,C       ; if STATUS.C == 1:
+      BCF PORTB,1        ;   PORTB[1] = 0
+    BTFSS STATUS,C       ; if STATUS.C == 0:
+      BSF PORTB,1        ;   PORTB[1] = 1
     MOVF JumpBack,W
-    DECFSZ LRAM_0x09,F
-      MOVWF PCL            ; !!Program-Counter-Modification
-    BSF LRAM_0x07,2
-    MOVWF PCL            ; !!Program-Counter-Modification
+    DECFSZ LRAM_0x09,F   ; LRAM_0x09 -= 1
+      MOVWF PCL          ; if LRAM_0x09 != 0: GOTO JumpBack
+    BSF LRAM_0x07,2      ; LRAM_0x07 |= 0'b0000_0100
+    MOVWF PCL            ; GOTO JumpBack
 LADR_0x00DF:
-    BCF PORTB,1
-    BCF LRAM_0x07,1
+    BCF PORTB,1          ; PORTB[1] = 0
+
+    BCF LRAM_0x07,1      ; LRAM_0x07 &= 0'b1111_1001
     BCF LRAM_0x07,2
-    CLRWDT
+
+    CLRWDT               ; reset watchdog
     MOVF JumpBack,W
-    MOVWF PCL            ; !!Program-Counter-Modification
+    MOVWF PCL            ; GOTO JumpBack
+
 LADR_0x00E5:
-    BTFSS TMR0,7
+    BTFSS TMR0,7         ; delay
       GOTO LADR_0x00E5
-LADR_0x00E7:
-    BTFSS TMR0,4
+LADR_0x00E7:             ; likely a timeout case from gotos above
+    BTFSS TMR0,4         ; if TMR0[4] == 0: GOTO LADR_0x00E7
       GOTO LADR_0x00E7
-    MOVLW 0x2E           ;   b'00101110'  d'046'  "."
+
+    MOVLW 0x2E           ; TMR0 = 0x2E
     MOVWF TMR0
-    BTFSC LRAM_0x07,1
-      GOTO LADR_0x00D3
-    BTFSS LRAM_0x07,4
-      GOTO LADR_0x00C3
+
+    BTFSC LRAM_0x07,1    ; if LRAM_0x07[1] == 1:
+      GOTO LADR_0x00D3   ;   GOTO LADR_0x00D3
+
+    BTFSS LRAM_0x07,4    ; if LRAM_0x07[4] == 0:
+      GOTO LADR_0x00C3   ;   GOTO LADR_0x00C3
+
     MOVF JumpBack,W
-    BTFSC LRAM_0x07,3
-      MOVWF PCL            ; !!Program-Counter-Modification
-    BTFSC PORTA,0
+    BTFSC LRAM_0x07,3    ; if LRAM_0x07[3] == 1:
+      MOVWF PCL          ;  GOTO JumpBack
+
+    BTFSC PORTA,0        ; if PORTA[0] == 0 OR PORTA[2] == 0:
       BTFSS PORTA,2
-        GOTO LADR_0x0006
+        GOTO LADR_0x0006 ;  GOTO LADR_0x0006
     GOTO LADR_0x0109
 
 ; wait for TMR0[7] to be set
@@ -337,7 +402,7 @@ LADR_0x00F8:
     BTFSC PORTB,0        ; if PORTB[0] == 1: GOTO LADR_0x00A2
       GOTO LADR_0x00A2
 
-    MOVF JumpBack,W     ; if LRAM_0x07[3] == 1: return to callback 
+    MOVF JumpBack,W     ; if LRAM_0x07[3] == 1: return to callback
     BTFSC LRAM_0x07,3
       MOVWF PCL
 
@@ -390,7 +455,7 @@ ResetVector: ; 0x011D
     MOVLW 0x0E           ; OPTION = b'00001110': T0CS=0 (CLKOUT), T0SE=0 (low->high transition), PSA=1 (WDT), PS<2:0>=110 (1:64)
     OPTION
 
-    MOVLW 0x2E           ; TMR0 = b'00101110'
+    MOVLW 0x2E           ; TMR0 = 0x2E
     MOVWF TMR0
 
 Initialize: ; 0x0127
